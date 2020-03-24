8322eea3c7be5586dfe2ddfa4ccbb9e80668ffa6
Filin <filin2009@gmail.com>
2020-03-24 18:09:44 +0200

    Update testDrawer.ts

--- js/ts/testDrawer.ts
+++ js/ts/testDrawer.ts
@@ -5,19 +5,20 @@
 export class TestDrawer {
 	draw() {
 		const body = $('body');
-         /*
+
 		const mustaView = { "SMSData": 	[
 			{"ID":3,"Name":"alphasms.ua","Ico":"/img/sms/alphasms.png"},
 			{"ID":25,"Name":"aramba.ru","Ico":"/img/sms/aramba.png"},
 			{"ID":21,"Name":"atompark.com","Ico":"/img/sms/atompark.png"}
 		]};
-        */
+
 		/*
 		const mustaView = fetch("./js/sms.js")
 			.then(function (resp) {
 				return resp.json();
 			});
          */
+		/*
 		let SMSData:  [{ Name: string , Ico: string }];
 		$.getJSON('sms.js', function (json) {
 			for (var key in json) {
@@ -32,6 +33,8 @@ export class TestDrawer {
 		});
 		const mustaView = {"SMSData": SMSData};
 		console.log(mustaView);
+		*/
+
 		body.append($.mustache(window.TemplateJS.test, mustaView));
 	}
 
